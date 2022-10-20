0f5cc607b4b93577f489ca45f44eb9a8d19e99a5
Hemanshu <upadhyayhri@gmail.com>
2022-10-20 19:30:47 +0530

    server files added

---
+++ client/build/precache-manifest.eaf042ac797a7d53860eadfd7b03ece3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8907f71c185d502d5b6608d7fefddcb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "411a01c736e4d8fe177f",
+    "url": "/static/css/main.9516a8fc.chunk.css"
+  },
+  {
+    "revision": "09c5175844b76237ca27",
+    "url": "/static/js/2.25815e50.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.25815e50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "411a01c736e4d8fe177f",
+    "url": "/static/js/main.51a9ba32.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/static/media/memoriesText.2b89c5fa.png"
+  }
+]);
